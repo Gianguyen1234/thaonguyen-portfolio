cffdfcc8847d6eea57cfc090efe31765737daed5
Thao Nguyen <nguyenluthao252@gmail.com>
2024-09-19 16:34:37 +0700

    cmt

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,9 @@
 export default function Footer() {
-    return (
+  return (
       <footer className="bg-gray-800 text-white py-4">
-        <div className="container mx-auto text-center">
-          <p>&copy; 2024 Thaonguyen's Portfolio. All rights reserved.</p>
-        </div>
+          <div className="container mx-auto text-center">
+              <p>&copy; 2024 Thaonguyen&apos;s Portfolio. All rights reserved.</p>
+          </div>
       </footer>
-    );
-  }
-  
+  );
+}
